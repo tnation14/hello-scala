d719485dcc3fd98be38d53d2a2a685f15731bcdf
Taylor Nation <tnation14@gmail.com>
2018-07-14 09:26:05 -0400

    Iterate over items in a list

--- helloworld/src/main/scala/Main.scala
+++ helloworld/src/main/scala/Main.scala
@@ -1,5 +1,15 @@
 import scala.util.parsing.combinator._
 
+
 object Main extends App {
-  println("Yerrr!")
+	val list: List[Any] = List(
+	  "Yerrr!",
+	  "a string",
+	  732,  // an integer
+	  'c',  // a character
+	  true, // a boolean value
+	  () => "an anonymous function returning a string"
+	)
+
+	list.foreach(element => println("Hello " + element))
 }
